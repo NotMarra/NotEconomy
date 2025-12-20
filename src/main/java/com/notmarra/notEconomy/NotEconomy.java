0ef81090918a55ff58502b4140f069338a3a0d5b
NotMarra <70058346+NotMarra@users.noreply.github.com>
2025-12-20 19:32:58 +0100

    init

---
+++ src/main/java/com/notmarra/notEconomy/NotEconomy.java
@@ -0,0 +1,17 @@
+package com.notmarra.notEconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NotEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
